191fd9b25b49d6580f81be1d610513464653f1f2
Shashank <shashank.041195@gmail.com>
2021-06-22 17:59:22 +0530

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState();
+  console.log(data);
+
+  useEffect(() => {
+    covidData();
+  }, []);
+
+  const covidData = async () => {
+    try {
+      const res = await fetch("https://api.covid19india.org/data.json");
+      const actualRes = await res.json();
+      // console.log(actualRes.statewise[0]);
+      setData(actualRes.statewise[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-container">
+        <h4 className="live">🔴 LIVE</h4>
+        <h1 className="heading">LIVE COVID-19 TRACKER</h1>
+        <div className="container">
+          <div className="card one">
+            <h1 className="title">country</h1>
+            <h2 className="data">india</h2>
+          </div>
+          <div className="card two">
+            <h1 className="title">OVERALL cases</h1>
+            <h2 className="data">{data.confirmed}</h2>
+          </div>
+          <div className="card three">
+            <h1 className="title">active cases</h1>
+            <h2 className="data">{data.active}</h2>
+          </div>
+          <div className="card four">
+            <h1 className="title">recovered</h1>
+            <h2 className="data">{data.recovered}</h2>
+          </div>
+          <div className="card five">
+            <h1 className="title">deaths</h1>
+            <h2 className="data">{data.deaths}</h2>
+          </div>
+          <div className="card six">
+            <h1 className="title">updated on</h1>
+            <h2 className="data">{data.lastupdatedtime}</h2>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <div class="footer-content">Design & Developed By @Shashank Pandey</div>
+      </footer>
+    </>
   );
 }
 
